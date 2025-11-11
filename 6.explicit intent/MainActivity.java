074285b1ff507587bc91390631266fd190217ed0
vijayalakshmiajithan3 <158257076+vijayalakshmiajithan3@users.noreply.github.com>
2025-11-11 13:44:03 +0530

    Update MainActivity.java

--- 6.explicit intent/MainActivity.java
+++ 6.explicit intent/MainActivity.java
@@ -1,28 +1,37 @@
-package com.example.explicitintent;
+package com.example.intent;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 
-
-
-
+import com.example.intent.R;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editText = findViewById(R.id.no);
     }
-    public void OnIndexSendButtonClicked(View view){
-        Intent intent=new Intent(this, MainActivity2.class);
-        EditText editText=(EditText) findViewById(R.id.no);
-        String index=editText.getText().toString();
-        intent.putExtra("ImageIndex",index);
+
+
+    public void OnIndexSendButtonClicked(View view) {
+    
+        Intent intent = new Intent(this, com.example.intent.MainActivity2.class);
+
+     
+        String index = editText.getText().toString();
+
+        intent.putExtra("ImageIndex", index);
+
+
         startActivity(intent);
     }
 }
+
